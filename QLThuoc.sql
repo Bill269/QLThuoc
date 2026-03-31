fd60785ba40eeaf9e2da0de96fdac45c424f7dd4
hoengggg <hoanglhth05020@gmail.com>
2026-03-31 21:33:24 +0700

    push sql again

--- QLThuoc.sql
+++ QLThuoc.sql
@@ -18,6 +18,7 @@ CREATE TABLE THUOC (
     TEN_THUOC NVARCHAR(255) NOT NULL,
     LOAI_THUOC NVARCHAR(50) NULL,
     SO_LUONG_TON INT NOT NULL DEFAULT 0,
+	NGAY_NHAP_THUOC DATE NOT NULL,
     HAN_SU_DUNG DATE NOT NULL
 );
 
@@ -45,12 +46,12 @@ VALUES
 
 -- 5. Chèn dữ liệu mẫu cho THUOC
 -- Lưu ý: KHÔNG chèn cột ID
-INSERT INTO THUOC (TEN_THUOC, LOAI_THUOC, SO_LUONG_TON, HAN_SU_DUNG) 
+INSERT INTO THUOC (TEN_THUOC, LOAI_THUOC, SO_LUONG_TON, NGAY_NHAP_THUOC, HAN_SU_DUNG) 
 VALUES 
-(N'Paracetamol', N'Giảm đau', 100, '2026-12-25'),
-(N'Amoxicillin', N'Kháng sinh', 50, '2028-01-01'),
-(N'Vitamin C', N'Vitamin', 200, '2027-06-15'),
-(N'Panadol Extra', N'Giảm đau', 150, '2025-05-20');
+(N'Paracetamol', N'Giảm đau', 100, '2026-03-29', '2026-12-25'),
+(N'Amoxicillin', N'Kháng sinh', 50, '2026-03-29','2028-01-01'),
+(N'Vitamin C', N'Vitamin', 200, '2026-03-29','2027-06-15'),
+(N'Panadol Extra', N'Giảm đau', 150, '2026-03-29','2025-05-20');
 
 -- 1. Tạo bảng Loại Thuốc
 CREATE TABLE LOAI_THUOC (
@@ -83,8 +84,8 @@ UPDATE THUOC SET GIA_BAN = 8000,  ID_LOAI = 2 WHERE TEN_THUOC = N'Panadol Extra'
 ALTER TABLE CHI_TIET_HOA_DON ADD GIA_LUC_BAN DECIMAL(18, 2);
 
 -- 4. Chèn thêm thuốc mới có đầy đủ giá
-INSERT INTO THUOC (TEN_THUOC, ID_LOAI, SO_LUONG_TON, HAN_SU_DUNG, GIA_BAN)
-VALUES (N'Decolgen', 2, 80, '2026-10-10', 12000);
+INSERT INTO THUOC (TEN_THUOC, ID_LOAI, SO_LUONG_TON, NGAY_NHAP_THUOC, HAN_SU_DUNG, GIA_BAN)
+VALUES (N'Decolgen', 2, 80, '2026-03-29', '2026-10-10', 12000);
 
 -- 5. Truy vấn kiểm tra tổng hợp (JOIN các bảng)
 SELECT 
@@ -93,6 +94,7 @@ SELECT
     L.TEN_LOAI, 
     T.GIA_BAN, 
     T.SO_LUONG_TON, 
+	T. NGAY_NHAP_THUOC,
     T.HAN_SU_DUNG
 FROM THUOC T
 LEFT JOIN LOAI_THUOC L ON T.ID_LOAI = L.ID;
@@ -142,3 +144,4 @@ WHERE TEN_THUOC = N'Amoxicillin';
 select * from  thuoc 
 select * from DON_VI_TINH
 
+select * from NGUOIDUNG
